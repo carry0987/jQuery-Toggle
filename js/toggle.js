a896fc5468cfb682ef172bea6fe8e363b2960f22
Zheng You Yun <carry0987@users.noreply.github.com>
2018-03-25 20:08:16 +0800

    Upload

---
+++ js/toggle.js
@@ -0,0 +1,17 @@
+// jQuery Toggle Script
+$(document).ready(function () {
+    //toggle the component with class accordion_body
+    $(".accordion_head").click(function () {
+        var tag = $(this).attr("node-tag");
+        if ($(this).next(".accordion_body").is(':visible')) {
+            $(this).next(".accordion_body").slideUp(300);
+            $(this).next(".accordion_body").addClass("collapsed");
+            Cookies.set('collapsed_Nodes', tag, { expires: 7, path: '' });
+            $(this).children(".plusminus").text('+');
+        } else {
+            $(this).next(".accordion_body").slideDown(300);
+            $(this).next(".accordion_body").addClass("expadned");
+            $(this).children(".plusminus").text('-');
+        }
+    });
+});
